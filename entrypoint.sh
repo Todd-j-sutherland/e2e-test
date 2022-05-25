7e201d6f0e01c758bef09617fceb0fc9c8e950fb
Todd Sutherland <toddsutherland@Ozeits-MacBook-Pro-94.local>
2022-05-25 13:44:28 +1000

    add docker file with java14

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 # npm install
-
+echo "todd------------------- $1 -----------------------------"
 ls
 echo "jeff------------------- $1 -----------------------------"
 time=$(date)
 echo "::set-output name=time::$time"
-npm run test
+# npm run test
 # npm install
 # npm run test
