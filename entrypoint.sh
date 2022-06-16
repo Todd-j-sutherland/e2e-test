5d11c95aefbce3c2344bff9d54d89c1a3ae741fa
AzureAD\ToddSutherland <Todd.Sutherland@nano.com.au>
2022-06-16 18:37:08 +1000

    111111

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@ ls
 pwd
 echo "jeff------------------- -----------------------------"
 echo "::set-output name=resultE2E:: SUCCESfffS"
-echo "action_state=black" >> $GITHUB_ENV
-echo "action_stateeeee=black" >> $GITHUB_ENV
+# echo "action_state=black" >> $GITHUB_ENV
+# echo "action_stateeeee=black" >> $GITHUB_ENV
+
+echo "::set-output name=message::TODD"
+echo "action_state=blue" >> $GITHUB_ENV
